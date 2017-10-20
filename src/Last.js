20c5ac767beb3e1e87b18c2ad79ebb4539064115
Deysi <sude.90@gmail.com>
2017-10-20 16:59:24 -0500

    poniendo estilos

--- src/Last.js
+++ src/Last.js
@@ -3,6 +3,7 @@ import SweetAlert from 'sweetalert-react';
 import 'sweetalert/dist/sweetalert.css';
 
 
+
 class LastStep extends Component {
 
     constructor(props) {
@@ -11,30 +12,35 @@ class LastStep extends Component {
             show: false
         }
     }
-   render(){
-    return (
-        <div className="container" >
-            <div className="row">
-        <div className="col-xs-12 col-lg-12 col-md-12 col-sm-12">
-            <h2>Yuhuuu!</h2>
-            <small>Click on <b>"Finish"</b>to join our community </small>
-        </div>
-        <div className="col-xs-12 col-lg-12 col-md-12 col-sm-12">
-            <button className="btn btn-back ">Back</button>   
-            <button onClick={() => this.setState({ show: true })}>Finish</button>
-            <SweetAlert
-            type='success'         
-              show={this.state.show}
-                         title="Good job!"
-              text="You clicked the finish button!"
-              
-              onConfirm={() => this.setState({ show: false })}
-            />
-        </div></div>
-</div>
-    )
-   }
+    render() {
+        return (
+            <div className="container" >
+                <div className="row">
+                    <div className="col-xs-12 col-lg-12 col-md-12 col-sm-12">
+                        <h1>Yuhuuu!</h1>
+                        <small className="su-small">Click on <b>"Finish"</b>to join our community </small>
+                    </div>
+
+                    <div className="col-xs-6 col-lg-6 col-md-6 col-sm- text-center">
+                        <button className="btn btn-back su-boton-black">Back</button>
+                    </div>
+                    <div className="col-xs-6 col-lg-6 col-md-6 col-sm-6 text-center">
+                        <button onClick={() => this.setState({ show: true })} className="btn btn-primary su-boton">Finish</button>
+                        <SweetAlert
+                            type='success'
+                            show={this.state.show}
+                            title="Good job!"
+                            text="You clicked the finish button!"
+
+                            onConfirm={() => this.setState({ show: false })}
+                        />
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
-   
+
 export default LastStep;
