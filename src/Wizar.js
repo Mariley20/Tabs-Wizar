54733a1aacc0331c6b6bd941d7bbbad3898e8678
KelyKley <kelyanamurohuamani@gmail.com>
2017-10-24 16:16:57 -0500

    arreglando los estilos de vistas

--- src/Wizar.js
+++ src/Wizar.js
@@ -6,20 +6,19 @@ import './Wizar.css'
 class Wizar extends React.Component {
     render(){
         return (
-<div className="container text-center">
-  <ul className="nav nav-tabs my-nav-Wizar">
-    <li className="active">
+<div id="wizzard" className="container text-center">
+  <ul id="eyeslash" className="nav nav-tabs my-nav-Wizar col-md-12 col-xs-12">
+    <li id="eyelash" className="active col-md-4 col-xs-4">
         <a data-toggle="tab" href="#info"><i className="glyphicon glyphicon-user"></i><br />
+        <span>Personal details</span></a>
+    </li>
+    <li id="eyelash" className="col-md-4 col-xs-4">  
+        <a data-toggle="tab" href="#perfil"><i className="fa fa-cog" aria-hidden="true"></i><br />
         <span>Additional Info</span></a>
     </li>
-    <li>
-        
-        <a data-toggle="tab" href="#perfil"><i class="fa fa-cog" aria-hidden="true"></i><br />
-        <span>Additional Info</span></a>
-    </li>
-    <li>
-        <a data-toggle="tab" href="#final"><i class="fa fa-check" aria-hidden="true"></i><br />
-        <span>Additional Info</span></a>
+    <li id="eyelash" className="col-md-4 col-xs-4">
+        <a data-toggle="tab" href="#final"><i className="fa fa-check" aria-hidden="true"></i><br />
+        <span>Last step</span></a>
     </li>
   </ul>
 
